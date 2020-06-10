c06e3f9a6e9553698d90d7c6babc806717c1923f
jisan39 <66712362+jisan39@users.noreply.github.com>
2020-06-10 10:39:11 +0600

    Update and rename zeroboy.py to jisantheblackpanther.py

--- jisantheblackpanther.py
+++ jisantheblackpanther.py
@@ -49,17 +49,17 @@ def jalan(z):
 ##### LOGO #####
 logo = """ -----------------------------•◈•
 (  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
-|__\~~) •||•RANA - RAJPUT---------------•◈•
-|__(-----\  •◈•------BLACK-TIGER--------•◈•
+|__(~)    •||•TEAM - CIVIL -BUSTERZZ ------•◈•
+|__\~~) •||•JISAN - AHMED---------------•◈•
+|__(-----\  •◈•------BLACK-PANTHER JISAN--------•◈•
 |__~~~\ •◈•-----█-------⑦-------█------•◈•
 |__~~~\ •◈•-----█-------⑧-------█------•◈•
 |__~~~\ •◈•-----█-------⑥-------█------•◈•
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mAhmed Jisan
+\033[1;96mInstagram \033[1;93m: \033[1;Flowxisan_ahmed_
+\033[1;96mFacebook  \033[1;93m: \033[1; fake.lovefuxk.u
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Jisanahmed/zero
 \033[1;91m======================================="""
 
 def tik():
@@ -80,15 +80,15 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92Black Panther
+\033[1;96mInstagram \033[1;93m: \033[1;92mFlowxisan_ahmed_
+\033[1;96mFacebook  \033[1;93m: \033[1;92m fake.lovefuxk.u
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Jisanahmed/zero
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "jisan"
+CorrectPassword = "jisan"
 
 loop = 'true'
 while (loop == 'true'):
